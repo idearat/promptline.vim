a815c500a2bd10ca801886ac115d0a94b2c57f19
idearat <ss@technicalpursuit.com>
2014-03-22 17:05:48 -0600

    Adjust computations and symbols.

--- autoload/promptline/slices/git_status.sh
+++ autoload/promptline/slices/git_status.sh
@@ -1,16 +1,17 @@
 function __promptline_git_status {
   [[ $(git rev-parse --is-inside-work-tree 2>/dev/null) == true ]] || return 1
 
-  local added_symbol="●"
-  local unmerged_symbol="✖"
-  local modified_symbol="✚"
+  local added_symbol="+"
+  local removed_symbol="-"
+  local unmerged_symbol="x"
+  local modified_symbol="~"
   local clean_symbol="✔"
-  local has_untracked_files_symbol="…"
+  local untracked_symbol="?"
 
   local ahead_symbol="↑"
   local behind_symbol="↓"
 
-  local unmerged_count=0 modified_count=0 has_untracked_files=0 added_count=0 is_clean=""
+  local unmerged_count=0 modified_count=0 untracked_count=0 added_count=0 removed_count=0 is_clean=""
 
   set -- $(git rev-list --left-right --count @{upstream}...HEAD 2>/dev/null)
   local behind_count=$1
@@ -19,22 +20,17 @@ function __promptline_git_status {
   # Added (A), Copied (C), Deleted (D), Modified (M), Renamed (R), changed (T), Unmerged (U), Unknown (X), Broken (B)
   while read line; do
     case "$line" in
+      AM*) added_count=$(( $added_count + 1 )) ;;
+      RM*) modified_count=$(( $modified_count + 1 )) ;;
+      D*) removed_count=$(( $removed_count + 1 )) ;;
       M*) modified_count=$(( $modified_count + 1 )) ;;
       U*) unmerged_count=$(( $unmerged_count + 1 )) ;;
+      A*) added_count=$(( $added_count + 1 )) ;;
+      ?*) untracked_count=$(( $untracked_count + 1 )) ;;
     esac
-  done < <(git diff --name-status)
+  done < <(git status --porcelain)
 
-  while read line; do
-    case "$line" in
-      *) added_count=$(( $added_count + 1 )) ;;
-    esac
-  done < <(git diff --name-status --cached)
-
-  if [ -n "$(git ls-files --others --exclude-standard)" ]; then
-    has_untracked_files=1
-  fi
-
-  if [ $(( unmerged_count + modified_count + has_untracked_files + added_count )) -eq 0 ]; then
+  if [ $(( removed_count + unmerged_count + modified_count + untracked_count + added_count )) -eq 0 ]; then
     is_clean=1
   fi
 
@@ -44,6 +40,8 @@ function __promptline_git_status {
   [[ $modified_count -gt 0 ]]      && { printf "%s" "$leading_whitespace$modified_symbol$modified_count"; leading_whitespace=" "; }
   [[ $unmerged_count -gt 0 ]]      && { printf "%s" "$leading_whitespace$unmerged_symbol$unmerged_count"; leading_whitespace=" "; }
   [[ $added_count -gt 0 ]]         && { printf "%s" "$leading_whitespace$added_symbol$added_count"; leading_whitespace=" "; }
-  [[ $has_untracked_files -gt 0 ]] && { printf "%s" "$leading_whitespace$has_untracked_files_symbol"; leading_whitespace=" "; }
+  [[ $removed_count -gt 0 ]]         && { printf "%s" "$leading_whitespace$removed_symbol$removed_count"; leading_whitespace=" "; }
+  [[ $untracked_count -gt 0 ]] && { printf "%s" "$leading_whitespace$untracked_symbol$untracked_count"; leading_whitespace=" "; }
   [[ $is_clean -gt 0 ]]            && { printf "%s" "$leading_whitespace$clean_symbol"; leading_whitespace=" "; }
 }
+
